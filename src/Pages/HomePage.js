29341120c9be7b0e901232556bf811c4f4237a0e
pedro sanchez <pedrosanchez707@gmail.com>
2022-05-27 00:07:56 -0700

    Added API

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,23 +1,36 @@
-import {Test} from '../JsonTest'
+import { useState } from "react";
 
-function Home(){
-    return(
-        <>
-        <div className="home-c">
-            {Test.map((item)=>{
-                return(
-                    <div className='card-c'>
-                        <div className='card-img'>
-                        {item.img}
-                        </div>
-                        <div className='card-dis'>
-                        {item.Discription}
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-        </>
-    )
+function Home() {
+  const [memes, setMemes] = useState([]);
+  const axios = require("axios");
+
+  axios
+    .get("http://ec2-3-139-63-18.us-east-2.compute.amazonaws.com/api/memes")
+    .then((res) => {
+      setMemes(res.data.memes);
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
+
+  return (
+    <>
+      <div className="home-c">
+        {memes.map((item) => {
+          return (
+            <a href={item.url} target="_blank">
+              <div className="card-c" key={item.id}>
+                <div className="card-img">
+                  <img src={item.url} alt="img" />
+                </div>
+                <div className="card-dis">
+                  <p>{item.name}</p>
+                </div>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+    </>
+  );
 }
-export default Home
+export default Home;
